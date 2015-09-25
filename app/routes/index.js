065efa5e89f3903fa4f32a5374711049c04ff01b
Alyssa Horrocks <am.rocks@outlook.com>
2015-09-25 13:33:46 -0400

    Allow user to delete a question.

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
+  },
+
+  actions: {
+    delete(question) {
+      if(confirm("Are you sure you want to delete?")) {
+        question.destroyRecord();
+        this.transitionTo('index');
+      } else {
+        this.transitionTo('index');
+      }
+    }
   }
 });
