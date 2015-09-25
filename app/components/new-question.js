49f685194140334bf204476291f1b0a117c9b27e
Alyssa Horrocks <am.rocks@outlook.com>
2015-09-25 14:09:29 -0400

    Allow user to add a new question to the page.

---
+++ app/components/new-question.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addQuestion: false,
+
+  actions: {
+    questionFormShow() {
+      this.set('addQuestion', true);
+    },
+    saveQuestion() {
+      var params = {
+        content: this.get('question'),
+        author: this.get('author'),
+        notes: this.get('notes')
+      }
+      this.sendAction('saveQuestion', params),
+      this.set('addQuestion', false);
+    }
+  }
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -13,6 +13,11 @@ export default Ember.Route.extend({
       } else {
         this.transitionTo('index');
       }
+    },
+    saveQuestion(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
     }
   }
 });
