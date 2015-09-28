c27816c59522c0c1405d55b1bfe2fc45835f78af
Alyssa Horrocks <am.rocks@outlook.com>
2015-09-27 20:58:57 -0400

    Allow user to submit answers and display them on the individual questions' page.

---
+++ app/components/new-answer.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addAnswer: false,
+
+  actions: {
+    answerFormShow() {
+      this.set('addAnswer', true);
+    },
+    submitAnswer() {
+      var params = {
+        name: this.get('name'),
+        text: this.get('text'),
+        question: this.get('question')
+      }
+      this.sendAction('submitAnswer', params),
+      this.set('addAnswer', false);
+    }
+  }
+});
